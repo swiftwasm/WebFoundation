d0d72594c496d30acbd8b64b3919f1949154aeba
Max Desiatov <max@desiatov.com>
2022-08-30 03:12:42 +0100

    Lower `Package.swift` requirement to Swift 5.5 (#1)
    * Lower `Package.swift` requirement to Swift 5.5
    
    This allows building with SwiftWasm 5.6, and hopefully makes it work with SwiftWasm 5.5 too.
    
    * Disable async tests in older SwiftWasm versions

--- Tests/WebFoundationTests/Networking/DownloadTests.swift
+++ Tests/WebFoundationTests/Networking/DownloadTests.swift
@@ -1,17 +1,20 @@
 import Foundation
-import XCTest
-import WebFoundation
-import JavaScriptKit
 import JavaScriptEventLoop
+import JavaScriptKit
+import WebFoundation
+import XCTest
 
 class DownloadTests: XCTestCase {
-	func testAsyncDownload() async throws {
-		let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
-		let (data, response) = try await URLSession.shared.data(from: url)
-		let posts = try JSONDecoder().decode([Post].self, from: data)
-		XCTAssertFalse(posts.isEmpty)
-		XCTAssertFalse(posts.first!.title.isEmpty)
-	}
+	// Async tests aren't supported in SwiftWasm 5.6 and earlier.
+	#if (os(WASI) && swift(>=5.7)) || canImport(Darwin)
+		func testAsyncDownload() async throws {
+			let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+			let (data, response) = try await URLSession.shared.data(from: url)
+			let posts = try JSONDecoder().decode([Post].self, from: data)
+			XCTAssertFalse(posts.isEmpty)
+			XCTAssertFalse(posts.first!.title.isEmpty)
+		}
+	#endif
 }
 
 struct Post: Decodable {
